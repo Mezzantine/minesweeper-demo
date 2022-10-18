9fcde3c0d00b978f75f3aa61ae3c854e7f202b70
Mezzantine <56084030+Mezzantine@users.noreply.github.com>
2022-10-18 10:07:15 +0200

    Empty mine field runs green, correct number of rows runs red

--- src/no/kristiania/minesweeper/MineFieldTest.java
+++ src/no/kristiania/minesweeper/MineFieldTest.java
@@ -9,8 +9,18 @@ public class MineFieldTest {
     void itShowsEmptyMinefield() {
         assertArrayEquals(new String[] { "000", "000" }, displayMineField(new String[] { "...", "..." }));
     }
+    @Test
+    void isShowsCorrectNumberOfRows() {
+        assertArrayEquals(
+          new String[] { "000", "000", "000" },
+          displayMineField(new String[] { "...", "...", "..." })
+        );
+    }
 
     private String[] displayMineField(String[] input) {
-        return new String[0];
+        //We do the simplest thing that could possibly work! For å få testen til å passere...
+        return new String[] {
+          "000", "000"
+        };
     }
 }
